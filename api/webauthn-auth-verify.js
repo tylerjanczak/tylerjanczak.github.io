d2f2394bfe75dff40814d324c593729bbace90d8
tylerjanczak <tnjanczak1@Outlook.com>
2026-08-02 04:39:43 -0500

    Update webauthn-auth-verify.js

--- api/webauthn-auth-verify.js
+++ api/webauthn-auth-verify.js
@@ -4,7 +4,6 @@ import { verifyAuthenticationResponse } from "@simplewebauthn/server";
 const RP_ID = "tylerjanczak-github-io.vercel.app";
 const ORIGIN = "https://tylerjanczak-github-io.vercel.app";
 const SESSION_TTL_SECONDS = 60 * 60 * 24; // 24 hours
-
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");

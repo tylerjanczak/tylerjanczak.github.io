1fa92eb3f41de34eb12c906e0f0d85600cdbb9df
tylerjanczak <tnjanczak1@Outlook.com>
2026-08-03 02:03:16 -0500

    Update chat-toggle.js

--- api/chat-toggle.js
+++ api/chat-toggle.js
@@ -1,5 +1,4 @@
 import { kv } from "@vercel/kv";
-
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
